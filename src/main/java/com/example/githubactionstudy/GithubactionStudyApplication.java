cb7feb14ac4a1cf3a8327922fe75f5dada2dc0c6
ycchoi <gks54410@gmail.com>
2022-11-12 22:23:43 +0900

    feat: sample-project 추가

---
+++ src/main/java/com/example/githubactionstudy/GithubactionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionStudyApplication.class, args);
+    }
+
+}
