a8dbc2231d3e3497d7fdc776d86a338f7898e466
ycchoi <gks54410@gmail.com>
2022-11-12 22:59:48 +0900

    feat: jar 파일명 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/github_action/build/libs/*-SNAPSHOT.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log" # Deploy Log 파일
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log" # Deploy Error Log 파일
